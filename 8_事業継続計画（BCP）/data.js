408936e917b7348c3e0d8dddf6e7af6cd830c1b4
takahashikatsunori <14124237+takahashikatsunori@users.noreply.github.com>
2026-05-06 09:06:08 +0900

    initial commit

---
+++ 8_事業継続計画（BCP）/data.js
@@ -0,0 +1,313 @@
+const BCP_DATA = {
+  name: "8_事業継続計画（BCP）",
+  icon: "🏢",
+  children: [
+    {
+      name: "1_行政BCP",
+      icon: "🏛️",
+      children: [
+        {
+          name: "1_都道府県",
+          icon: "🗾",
+          children: [
+            { name: "01_北海道", files: [], children: [] },
+            { name: "02_青森県", files: [], children: [] },
+            { name: "03_岩手県", files: [], children: [] },
+            { name: "04_宮城県", files: [], children: [] },
+            { name: "05_秋田県", files: [], children: [] },
+            { name: "06_山形県", files: [], children: [] },
+            { name: "07_福島県", files: [], children: [] },
+            { name: "08_茨城県", files: [], children: [] },
+            { name: "09_栃木県", files: [], children: [] },
+            { name: "10_群馬県", files: [], children: [] },
+            { name: "11_埼玉県", files: [], children: [] },
+            { name: "12_千葉県", files: [], children: [] },
+            { name: "13_東京都", files: [{ name: "東京都業務継続計画.pdf", path: "1_行政BCP/1_都道府県/13_東京都/東京都業務継続計画.pdf", icon: "📕" }], children: [] },
+            { name: "14_神奈川県", files: [], children: [] },
+            { name: "15_新潟県", files: [], children: [] },
+            { name: "16_富山県", files: [], children: [] },
+            { name: "17_石川県", files: [], children: [] },
+            { name: "18_福井県", files: [], children: [] },
+            { name: "19_山梨県", files: [], children: [] },
+            { name: "20_長野県", files: [], children: [] },
+            { name: "21_岐阜県", files: [], children: [] },
+            { name: "22_静岡県", files: [], children: [] },
+            { name: "23_愛知県", files: [{ name: "愛知県業務継続計画.pdf", path: "1_行政BCP/1_都道府県/23_愛知県/愛知県業務継続計画.pdf", icon: "📕" }], children: [] },
+            { name: "24_三重県", files: [], children: [] },
+            { name: "25_滋賀県", files: [], children: [] },
+            { name: "26_京都府", files: [], children: [] },
+            { name: "27_大阪府", files: [{ name: "大阪府業務継続計画.pdf", path: "1_行政BCP/1_都道府県/27_大阪府/大阪府業務継続計画.pdf", icon: "📕" }], children: [] },
+            { name: "28_兵庫県", files: [{ name: "兵庫県業務継続計画.pdf", path: "1_行政BCP/1_都道府県/28_兵庫県/兵庫県業務継続計画.pdf", icon: "📕" }], children: [] },
+            { name: "29_奈良県", files: [], children: [] },
+            { name: "30_和歌山県", files: [], children: [] },
+            { name: "31_鳥取県", files: [], children: [] },
+            { name: "32_島根県", files: [], children: [] },
+            { name: "33_岡山県", files: [], children: [] },
+            { name: "34_広島県", files: [{ name: "広島県業務継続計画.pdf", path: "1_行政BCP/1_都道府県/34_広島県/広島県業務継続計画.pdf", icon: "📕" }], children: [] },
+            { name: "35_山口県", files: [], children: [] },
+            { name: "36_徳島県", files: [], children: [] },
+            { name: "37_香川県", files: [], children: [] },
+            { name: "38_愛媛県", files: [], children: [] },
+            { name: "39_高知県", files: [], children: [] },
+            { name: "40_福岡県", files: [{ name: "福岡県業務継続計画.pdf", path: "1_行政BCP/1_都道府県/40_福岡県/福岡県業務継続計画.pdf", icon: "📕" }], children: [] },
+            { name: "41_佐賀県", files: [], children: [] },
+            { name: "42_長崎県", files: [], children: [] },
+            { name: "43_熊本県", files: [], children: [] },
+            { name: "44_大分県", files: [], children: [] },
+            { name: "45_宮崎県", files: [], children: [] },
+            { name: "46_鹿児島県", files: [], children: [] },
+            { name: "47_沖縄県", files: [], children: [] }
+          ]
+        },
+        {
+          name: "2_市区町村",
+          icon: "🏙️",
+          children: [
+            {
+              name: "東京都",
+              icon: "🗼",
+              children: [
+                // 特別区（23区）
+                { name: "千代田区", files: [], children: [] },
+                { name: "中央区", files: [], children: [] },
+                { name: "港区", files: [], children: [] },
+                { name: "新宿区", files: [{ name: "新宿区事業継続計画.pdf", path: "1_行政BCP/2_市区町村/東京都/新宿区/新宿区事業継続計画.pdf", icon: "📕" }], children: [] },
+                { name: "文京区", files: [], children: [] },
+                { name: "台東区", files: [], children: [] },
+                { name: "墨田区", files: [], children: [] },
+                { name: "江東区", files: [], children: [] },
+                { name: "品川区", files: [], children: [] },
+                { name: "目黒区", files: [], children: [] },
+                { name: "大田区", files: [], children: [] },
+                { name: "世田谷区", files: [], children: [] },
+                { name: "渋谷区", files: [], children: [] },
+                { name: "中野区", files: [], children: [] },
+                { name: "杉並区", files: [], children: [] },
+                { name: "豊島区", files: [], children: [] },
+                { name: "北区", files: [], children: [] },
+                { name: "荒川区", files: [], children: [] },
+                { name: "板橋区", files: [], children: [] },
+                { name: "練馬区", files: [], children: [] },
+                { name: "足立区", files: [], children: [] },
+                { name: "葛飾区", files: [], children: [] },
+                { name: "江戸川区", files: [], children: [] },
+                // 市部
+                { name: "八王子市", files: [], children: [] },
+                { name: "立川市", files: [], children: [] },
+                { name: "武蔵野市", files: [], children: [] },
+                { name: "三鷹市", files: [], children: [] },
+                { name: "青梅市", files: [], children: [] },
+                { name: "府中市", files: [], children: [] },
+                { name: "昭島市", files: [], children: [] },
+                { name: "調布市", files: [], children: [] },
+                { name: "町田市", files: [], children: [] },
+                { name: "小金井市", files: [], children: [] },
+                { name: "小平市", files: [{ name: "小平市業務継続計画.pdf", path: "1_行政BCP/2_市区町村/東京都/小平市/小平市業務継続計画.pdf", icon: "📕" }], children: [] },
+                { name: "日野市", files: [], children: [] },
+                { name: "東村山市", files: [], children: [] },
+                { name: "国分寺市", files: [], children: [] },
+                { name: "国立市", files: [], children: [] },
+                { name: "福生市", files: [], children: [] },
+                { name: "狛江市", files: [], children: [] },
+                { name: "東大和市", files: [], children: [] },
+                { name: "清瀬市", files: [], children: [] },
+                { name: "東久留米市", files: [], children: [] },
+                { name: "武蔵村山市", files: [], children: [] },
+                { name: "多摩市", files: [], children: [] },
+                { name: "稲城市", files: [], children: [] },
+                { name: "羽村市", files: [], children: [] },
+                { name: "あきる野市", files: [], children: [] },
+                { name: "西東京市", files: [], children: [] },
+                // 町村部
+                { name: "瑞穂町", files: [], children: [] },
+                { name: "日の出町", files: [], children: [] },
+                { name: "檜原村", files: [], children: [] },
+                { name: "奥多摩町", files: [], children: [] },
+                // 島しょ部
+                { name: "大島町", files: [], children: [] },
+                { name: "利島村", files: [], children: [] },
+                { name: "新島村", files: [], children: [] },
+                { name: "神津島村", files: [], children: [] },
+                { name: "三宅村", files: [], children: [] },
+                { name: "御蔵島村", files: [], children: [] },
+                { name: "八丈町", files: [], children: [] },
+                { name: "青ヶ島村", files: [], children: [] },
+                { name: "小笠原村", files: [], children: [] }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "2_企業BCP",
+      icon: "🏭",
+      children: [
+        {
+          name: "1_製造業",
+          icon: "⚙️",
+          children: [
+            { name: "トヨタ自動車株式会社", files: [], children: [] },
+            { name: "本田技研工業株式会社", files: [], children: [] },
+            { name: "日産自動車株式会社", files: [], children: [] },
+            { name: "株式会社デンソー", files: [], children: [] },
+            { name: "パナソニック株式会社", files: [], children: [] },
+            { name: "ソニーグループ株式会社", files: [], children: [] },
+            { name: "株式会社日立製作所", files: [], children: [] },
+            { name: "三菱電機株式会社", files: [], children: [] },
+            { name: "株式会社東芝", files: [], children: [] },
+            { name: "富士フイルム株式会社", files: [], children: [] },
+            { name: "キヤノン株式会社", files: [], children: [] },
+            { name: "株式会社リコー", files: [], children: [] },
+            { name: "コニカミノルタ株式会社", files: [], children: [] },
+            { name: "旭化成株式会社", files: [], children: [] },
+            { name: "信越化学工業株式会社", files: [], children: [] },
+            { name: "住友化学株式会社", files: [], children: [] },
+            { name: "三菱ケミカルグループ株式会社", files: [], children: [] },
+            { name: "株式会社JSR", files: [], children: [] },
+            { name: "AGC株式会社", files: [], children: [] },
+            { name: "新日本製鐵株式会社（日本製鉄）", files: [], children: [] },
+            { name: "JFEスチール株式会社", files: [], children: [] },
+            { name: "住友金属鉱山株式会社", files: [], children: [] },
+            { name: "三菱重工業株式会社", files: [], children: [] },
+            { name: "川崎重工業株式会社", files: [], children: [] },
+            { name: "IHI株式会社", files: [], children: [] },
+            { name: "株式会社小松製作所", files: [], children: [] },
+            { name: "株式会社クボタ", files: [], children: [] },
+            { name: "ダイキン工業株式会社", files: [], children: [] }
+          ]
+        },
+        {
+          name: "2_情報通信・IT",
+          icon: "💻",
+          children: [
+            { name: "富士通株式会社", files: [{ name: "富士通レポート2025.pdf", path: "2_企業BCP/2_情報通信・IT/富士通株式会社/富士通レポート2025.pdf", icon: "📊" }], children: [] },
+            { name: "NTTデータグループ株式会社", files: [{ name: "NTTグループ_統合報告書2024.pdf", path: "2_企業BCP/2_情報通信・IT/NTTデータグループ株式会社/NTTグループ_統合報告書2024.pdf", icon: "📊" }], children: [] },
+            { name: "日本電気株式会社（NEC）", files: [{ name: "NEC_ESGデータブック2024.pdf", path: "2_企業BCP/2_情報通信・IT/日本電気株式会社（NEC）/NEC_ESGデータブック2024.pdf", icon: "📊" }], children: [] },
+            { name: "株式会社野村総合研究所", files: [], children: [] },
+            { name: "株式会社オービック", files: [], children: [] },
+            { name: "BIPROGY株式会社", files: [], children: [] },
+            { name: "株式会社日立ソリューションズ", files: [], children: [] },
+            { name: "TIS株式会社", files: [], children: [] },
+            { name: "伊藤忠テクノソリューションズ株式会社", files: [], children: [] },
+            { name: "株式会社SCSK", files: [], children: [] },
+            { name: "キーエンス株式会社", files: [], children: [] }
+          ]
+        },
+        {
+          name: "3_金融・保険",
+          icon: "🏦",
+          children: [
+            { name: "株式会社三菱UFJ銀行", files: [{ name: "MUFG_統合報告書2025.pdf", path: "2_企業BCP/3_金融・保険/株式会社三菱UFJ銀行/MUFG_統合報告書2025.pdf", icon: "📊" }], children: [] },
+            { name: "株式会社三井住友銀行", files: [{ name: "SMBCグループレポート2025.pdf", path: "2_企業BCP/3_金融・保険/株式会社三井住友銀行/SMBCグループレポート2025.pdf", icon: "📊" }], children: [] },
+            { name: "株式会社みずほ銀行", files: [], children: [] },
+            { name: "株式会社りそな銀行", files: [], children: [] },
+            { name: "野村ホールディングス株式会社", files: [], children: [] },
+            { name: "大和証券グループ本社株式会社", files: [], children: [] },
+            { name: "東京海上ホールディングス株式会社", files: [], children: [] },
+            { name: "MS&ADインシュアランスグループホールディングス株式会社", files: [], children: [] },
+            { name: "SOMPOホールディングス株式会社", files: [], children: [] },
+            { name: "第一生命ホールディングス株式会社", files: [], children: [] },
+            { name: "日本生命保険相互会社", files: [], children: [] },
+            { name: "明治安田生命保険相互会社", files: [], children: [] },
+            { name: "住友生命保険相互会社", files: [], children: [] }
+          ]
+        },
+        {
+          name: "4_流通・小売",
+          icon: "🛒",
+          children: [
+            { name: "株式会社セブン&アイ・ホールディングス", files: [{ name: "セブンアイ_経営レポート2025.pdf", path: "2_企業BCP/4_流通・小売/株式会社セブン&アイ・ホールディングス/セブンアイ_経営レポート2025.pdf", icon: "📊" }], children: [] },
+            { name: "イオン株式会社", files: [{ name: "イオン_サステナビリティデータ集2024.pdf", path: "2_企業BCP/4_流通・小売/イオン株式会社/イオン_サステナビリティデータ集2024.pdf", icon: "📊" }], children: [] },
+            { name: "株式会社ファーストリテイリング", files: [], children: [] },
+            { name: "株式会社良品計画", files: [], children: [] },
+            { name: "株式会社ニトリホールディングス", files: [], children: [] },
+            { name: "株式会社ヤマダホールディングス", files: [], children: [] },
+            { name: "株式会社ビックカメラ", files: [], children: [] },
+            { name: "株式会社高島屋", files: [], children: [] },
+            { name: "株式会社三越伊勢丹ホールディングス", files: [], children: [] }
+          ]
+        },
+        {
+          name: "5_建設・不動産",
+          icon: "🏗️",
+          children: [
+            { name: "大成建設株式会社", files: [{ name: "大成建設_統合報告書2025.pdf", path: "2_企業BCP/5_建設・不動産/大成建設株式会社/大成建設_統合報告書2025.pdf", icon: "📊" }], children: [] },
+            { name: "鹿島建設株式会社", files: [{ name: "鹿島建設_統合報告書2025.pdf", path: "2_企業BCP/5_建設・不動産/鹿島建設株式会社/鹿島建設_統合報告書2025.pdf", icon: "📊" }], children: [] },
+            { name: "清水建設株式会社", files: [], children: [] },
+            { name: "株式会社大林組", files: [], children: [] },
+            { name: "竹中工務店", files: [], children: [] },
+            { name: "株式会社長谷工コーポレーション", files: [], children: [] },
+            { name: "三井不動産株式会社", files: [], children: [] },
+            { name: "住友不動産株式会社", files: [], children: [] },
+            { name: "東急不動産ホールディングス株式会社", files: [], children: [] },
+            { name: "野村不動産ホールディングス株式会社", files: [], children: [] }
+          ]
+        },
+        {
+          name: "6_運輸・物流",
+          icon: "✈️",
+          children: [
+            { name: "株式会社日本航空（JAL）", files: [], children: [] },
+            { name: "全日本空輸株式会社（ANA）", files: [], children: [] },
+            { name: "東日本旅客鉄道株式会社（JR東日本）", files: [], children: [] },
+            { name: "東海旅客鉄道株式会社（JR東海）", files: [], children: [] },
+            { name: "西日本旅客鉄道株式会社（JR西日本）", files: [], children: [] },
+            { name: "小田急電鉄株式会社", files: [], children: [] },
+            { name: "東急株式会社", files: [], children: [] },
+            { name: "ヤマトホールディングス株式会社", files: [], children: [] },
+            { name: "株式会社日本通運", files: [], children: [] },
+            { name: "SGホールディングス株式会社", files: [], children: [] },
+            { name: "福山通運株式会社", files: [], children: [] },
+            { name: "株式会社センコーグループホールディングス", files: [], children: [] }
+          ]
+        },
+        {
+          name: "7_エネルギー",
+          icon: "⚡",
+          children: [
+            { name: "ENEOSホールディングス株式会社", files: [{ name: "ENEOSレポート2025.pdf", path: "2_企業BCP/7_エネルギー/ENEOSホールディングス株式会社/ENEOSレポート2025.pdf", icon: "📊" }], children: [] },
+            { name: "出光興産株式会社", files: [], children: [] },
+            { name: "コスモエネルギーホールディングス株式会社", files: [], children: [] },
+            { name: "東京電力ホールディングス株式会社", files: [{ name: "東京電力_防災業務計画2024.pdf", path: "2_企業BCP/7_エネルギー/東京電力ホールディングス株式会社/東京電力_防災業務計画2024.pdf", icon: "📕" }], children: [] },
+            { name: "関西電力株式会社", files: [], children: [] },
+            { name: "中部電力株式会社", files: [], children: [] },
+            { name: "九州電力株式会社", files: [], children: [] },
+            { name: "東北電力株式会社", files: [], children: [] },
+            { name: "北海道電力株式会社", files: [], children: [] },
+            { name: "東京ガス株式会社", files: [], children: [] },
+            { name: "大阪ガス株式会社", files: [], children: [] },
+            { name: "東邦ガス株式会社", files: [], children: [] }
+          ]
+        },
+        {
+          name: "8_食品・飲料",
+          icon: "🍱",
+          children: [
+            { name: "味の素株式会社", files: [{ name: "味の素_サステナビリティレポート2025.pdf", path: "2_企業BCP/8_食品・飲料/味の素株式会社/味の素_サステナビリティレポート2025.pdf", icon: "📊" }], children: [] },
+            { name: "明治ホールディングス株式会社", files: [{ name: "明治グループサステナビリティ報告2025.pdf", path: "2_企業BCP/8_食品・飲料/明治ホールディングス株式会社/明治グループサステナビリティ報告2025.pdf", icon: "📊" }], children: [] },
+            { name: "株式会社日清食品ホールディングス", files: [], children: [] },
+            { name: "キリンホールディングス株式会社", files: [], children: [] },
+            { name: "アサヒグループホールディングス株式会社", files: [], children: [] },
+            { name: "サントリーホールディングス株式会社", files: [], children: [] },
+            { name: "森永製菓株式会社", files: [], children: [] },
+            { name: "江崎グリコ株式会社", files: [], children: [] }
+          ]
+        },
+        {
+          name: "9_医薬品・医療",
+          icon: "💊",
+          children: [
+            { name: "武田薬品工業株式会社", files: [{ name: "武田薬品_統合報告書2025.pdf", path: "2_企業BCP/9_医薬品・医療/武田薬品工業株式会社/武田薬品_統合報告書2025.pdf", icon: "📊" }], children: [] },
+            { name: "アステラス製薬株式会社", files: [], children: [] },
+            { name: "第一三共株式会社", files: [], children: [] },
+            { name: "塩野義製薬株式会社", files: [], children: [] },
+            { name: "大塚ホールディングス株式会社", files: [], children: [] },
+            { name: "エーザイ株式会社", files: [], children: [] },
+            { name: "株式会社メディカルシステムネットワーク", files: [], children: [] }
+          ]
+        }
+      ]
+    }
+  ]
+};
